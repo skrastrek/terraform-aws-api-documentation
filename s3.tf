91f15d649207403a0503df4c8833b1015d67bfce
Sebastian Ramsland <SebRamsland@users.noreply.github.com>
2026-06-03 13:33:22 +0200

    fix: Remove usage of deprecated attribute (#5)

--- s3.tf
+++ s3.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = "${var.name_prefix}-documentation-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.id}"
+  bucket = "${var.name_prefix}-documentation-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
 
   tags = var.tags
 }
